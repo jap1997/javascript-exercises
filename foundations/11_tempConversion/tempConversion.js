5ad23bcc78941a377a019cacad205adfda33bfe0
Japneet Singh <55660990+jap1997@users.noreply.github.com>
2026-09-07 13:07:52 +0200

    excercise 11_tempConversion from foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhenheitTemp) {
+  const res = (farhenheitTemp - 32) * 5/9
+  return Number(res.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const res = (celsiusTemp * 9/5 + 32) 
+  return Number(res.toFixed(1))
 };
 
 // Do not edit below this line
