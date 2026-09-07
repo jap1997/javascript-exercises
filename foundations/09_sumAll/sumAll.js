fdcd2477f5fc1125bae4320bc1f5e315a318480a
Japneet Singh <55660990+jap1997@users.noreply.github.com>
2026-09-07 12:37:09 +0200

    excercisie 09_sumAll of foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number" ||
+        a < 0 || b < 0 ||
+        parseInt(a) !== a || parseInt(b) !== b) return "ERROR"
+    let res = 0
+    let min = (a > b) ? b : a
+    let max = (a > b) ? a : b
 
+    for (let i = min; i <= max; i += 1) {
+        res += i
+    }
+    return res
 };
 
 // Do not edit below this line
