a3db6464c258beb5505b8a71069064b9b84b2ad3
Japneet Singh <55660990+jap1997@users.noreply.github.com>
2026-09-07 12:52:46 +0200

    excercise 10_leapYears of foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let res = false
+    if (year % 4 == 0) res = true
+    if (year % 100 == 0 && year % 400 !== 0) res = false
+    return res
 };
 
 // Do not edit below this line
