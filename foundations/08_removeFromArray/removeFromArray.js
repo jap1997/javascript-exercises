a08441fcdc8094c0666163a91a20c15036f733bf
Japneet Singh <55660990+jap1997@users.noreply.github.com>
2026-09-07 12:24:03 +0200

    excercie 08_removeFromArray of foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let res = [...array]
+    const values = Object.values(arguments).slice(1)
+    values.forEach(v => {
+        res = res.filter(el => el !== v)
+    })
+    return res
 };
 
 // Do not edit below this line
